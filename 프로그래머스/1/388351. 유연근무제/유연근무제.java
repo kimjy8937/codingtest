5caad2fa6cb83e512bdbfded3d554c144ddf0c9e
Kim JinYoung <42676165+kimjy8937@users.noreply.github.com>
2025-02-18 19:24:52 +0900

    [level 1] Title: 유연근무제, Time: 0.90 ms, Memory: 87.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int[] schedules, int[][] timelogs, int startday) {
+        int answer = 0;
+     
+        for(int i = 0; i<timelogs.length; i++){
+            int check = 5;
+            for(int j = 0; j<timelogs[i].length; j++){
+                int schedule = (schedules[i] / 100 * 60) + (schedules[i] % 100);
+                int timelog = (timelogs[i][j] / 100) * 60 + (timelogs[i][j] % 100);
+                if(schedule + 10 < timelog){
+                    if(startday+j == 6 || startday+j == 7 || startday+j == 13)
+                        continue;
+                    check -= 1;
+                }else if(schedule >= 2350){
+                    if(timelog < 10 && timelog + 2400 > schedule + 10){
+                        if(startday+j == 6 || startday+j == 7 || startday+j == 13)
+                        continue;
+                    }
+                    check -= 1;
+                }
+            }
+            if(check == 5)
+                answer += 1;
+        }
+        return answer;
+    }
+}
