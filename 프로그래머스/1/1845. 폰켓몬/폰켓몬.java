bfcc9340136c9b79df0e9e81c674db4a2c9b117f
Kim JinYoung <42676165+kimjy8937@users.noreply.github.com>
2025-02-19 17:03:06 +0900

    [level 1] Title: 폰켓몬, Time: 6.02 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,24 +1,13 @@
-import java.util.Map;
-import java.util.HashMap;
+import java.util.Set;
+import java.util.HashSet;
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-        int count = 0;
-        Map<Integer, Integer> hm = new HashMap<Integer, Integer>();
+        Set<Integer> hs = new HashSet<>();
         for(int i = 0; i< nums.length; i++){
-            if(hm.get(nums[i]) == null){
-                hm.put(nums[i], 1);
-                count += 1;
-            }else{
-                hm.put(nums[i], hm.get(nums[i]) + 1);
-            }            
+            hs.add(nums[i]);
         }
-        
-        if(count <= nums.length/2)
-            answer =  count;
-        else 
-            answer = nums.length / 2;
-        
-        return answer;
+
+        return hs.size()>nums.length/2 ? nums.length/2 : hs.size();
     }
 }
