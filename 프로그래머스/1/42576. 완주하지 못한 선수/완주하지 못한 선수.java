49a6586ee84f8ecd7ae6d0bb29d1b504735ef39e
Kim JinYoung <42676165+kimjy8937@users.noreply.github.com>
2025-02-25 17:10:11 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 87.38 ms, Memory: 96.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,23 +4,21 @@ import java.util.HashMap;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Map<String, Integer> map = new HashMap<String, Integer>();
-        for(int i = 0; i < participant.length; i++){
-            if(map.get(participant[i]) != null){
-                map.put(participant[i], map.get(participant[i]) + 1);
-            }else
-                map.put(participant[i], 1);
+        Map<String, Integer> map = new HashMap<>();
+        for (String s : participant) {
+            map.merge(s, 1, Integer::sum);
         }
-        
+
         for(int i = 0; i< completion.length; i++){
-            map.put(participant[i], map.get(participant[i]) - 1);
+            map.put(completion[i], map.get(completion[i]) - 1);
+        }
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if(entry.getValue() > 0){
+                return entry.getKey();
+            }
         }
         
-        for(Entry<String, Integer> entry : map.entrySet()){
-            if(entry.getValue() >= 1)
-                return entry.getKey();
-        }
         return " ";
     }
-    
 }
