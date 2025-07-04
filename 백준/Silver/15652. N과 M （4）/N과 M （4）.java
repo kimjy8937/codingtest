31629d1028dcfffb7e30c7844f825039f9ace999
Kim JinYoung <42676165+kimjy8937@users.noreply.github.com>
2025-07-04 19:38:51 +0900

    [Silver III] Title: N과 M (4), Time: 128 ms, Memory: 17216 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    static int N, M;
+    static int arr[];
+    static StringBuilder sb;
+    
+    static void func(int k){
+        if(k == M){
+            for(int i = 0; i < M; i++){
+                sb.append(arr[i]).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+        for(int i = 1; i <= N; i++){
+            if(k == 0 || (k > 0 && arr[k-1] <= i)){
+                arr[k] = i;
+                func(k + 1);
+            }
+        }
+    }
+    
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+        
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        
+        arr = new int[10];
+        
+        func(0);
+        System.out.println(sb.toString());
+    }
+}
