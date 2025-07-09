fe5929589603880cce2ce0050709eadae2e8cd4e
Kim JinYoung <42676165+kimjy8937@users.noreply.github.com>
2025-07-09 20:11:40 +0900

    [Silver II] Title: N과 M (10), Time: 108 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N, M;
+    static int[] numArr;
+    static int[] arr;
+    static boolean[] vis;
+    static StringBuilder sb;
+   
+    static void func(int k){
+        if(k == M){
+            for(int i = 0; i < M; i++){
+                sb.append(arr[i]).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+        
+        int prev = -1;
+        for(int i = 0; i < N; i++){
+            if(!vis[i]){
+                if(k == 0 || (k > 0 && numArr[i] >= arr[k-1])){
+                    if(numArr[i] == prev) continue;
+                    prev = numArr[i];
+                    
+                    vis[i] = true;
+                    arr[k] = numArr[i]; 
+                    func(k + 1);
+                    vis[i] = false;
+                } 
+            }
+        }
+    }
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+        
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        
+        st = new StringTokenizer(br.readLine());
+      
+        numArr = new int[N];
+        arr = new int[M];
+        vis = new boolean[N];
+        
+        for(int i = 0; i < N; i++){
+            numArr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(numArr);
+        
+        func(0);
+        System.out.println(sb.toString());
+    }
+}
