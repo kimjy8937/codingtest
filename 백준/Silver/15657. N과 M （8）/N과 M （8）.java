c58fd55f96dfde334cd02e27f393c3a962ed7b16
Kim JinYoung <42676165+kimjy8937@users.noreply.github.com>
2025-07-14 01:09:32 +0900

    [Silver III] Title: N과 M (8), Time: 156 ms, Memory: 17980 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N, M;
+    static int[] numArr;
+    static int[] arr;
+    static StringBuilder sb;
+   
+    static void func(int k){
+        if(k == M){
+            for(int i = 0; i < M; i++){
+                sb.append(arr[i]).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+        
+        for(int i = 0; i < N; i++){
+            if(k == 0 || (k > 0 && arr[k-1] <= numArr[i])){
+                arr[k] = numArr[i]; 
+                func(k + 1); 
+            }
+        }
+    }
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+        
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        
+        st = new StringTokenizer(br.readLine());
+      
+        numArr = new int[N];
+        arr = new int[M];
+        
+        for(int i = 0; i < N; i++){
+            numArr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(numArr);
+        
+        func(0);
+        System.out.println(sb.toString());
+    }
+}
