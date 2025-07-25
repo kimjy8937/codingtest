2520405a5ea155a8425372cc630d5c14cde19121
Kim JinYoung <42676165+kimjy8937@users.noreply.github.com>
2025-07-25 13:25:11 +0900

    [level 2] Title: 타겟 넘버, Time: 4.43 ms, Memory: 84.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,23 @@
+class Solution {
+    static int count = 0;
+    public int solution(int[] numbers, int target) {
+        dfs(numbers, 0, target, 0);
+        return count;
+    }
+    public void dfs(int[] numbers, int depth, int target, int result){
+        if (depth == numbers.length){ 
+            if (target == result){
+                count++;
+            }
+            return;
+        }
+        
+        int plus = result + numbers[depth];
+        int minus = result - numbers[depth];
+        
+        dfs(numbers, depth+1, target, plus);
+        dfs(numbers, depth+1, target, minus);
+        
+    }
+    
+}
