4aadeae9baea136fe44021a704b4cc5b8f2d1b1c
Kim JinYoung <42676165+kimjy8937@users.noreply.github.com>
2025-07-29 12:33:40 +0900

    [Silver I] Title: 정수 삼각형, Time: 232 ms, Memory: 25056 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -1,33 +1,28 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[][] triangle = new int[N + 1][N + 1];
-        int[][] dp = new int[N + 1][N + 1];
+        StringTokenizer st;
 
-        for (int i = 1; i <= N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= N; j++) {
+        int n = Integer.parseInt(br.readLine());
+        int[][] triangle = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j <= i; j++) {
                 triangle[i][j] = Integer.parseInt(st.nextToken());
-
-                dp[i][j] = Math.max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j];
-
-                if(!st.hasMoreTokens()){
-                    break;
-                }
             }
         }
-        int max = -1;
-        for(int i = 1; i <= N; i++) {
-            if(max < dp[N][i])
-                max = dp[N][i];
+
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                triangle[i][j] += Math.max(triangle[i + 1][j], triangle[i + 1][j + 1]);
+            }
         }
 
-        System.out.println(max);
+        System.out.println(triangle[0][0]); 
     }
 }
