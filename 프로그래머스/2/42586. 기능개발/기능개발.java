1a9df477d9fed7d8e00d3272d8d3cd83042af309
Kim JinYoung <42676165+kimjy8937@users.noreply.github.com>
2025-08-13 16:45:35 +0900

    [level 2] Title: 기능개발, Time: 0.48 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,39 +1,6 @@
-import java.util.*;
-import java.io.*;
-
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int[] answer;
-        Queue<Integer> que = new ArrayDeque<>();
-        Queue<Integer> speedQue = new ArrayDeque<>();
-        ArrayList<Integer> list = new ArrayList();
-        int day = 1;
-        
-        for(int i = 0; i < progresses.length; i++){
-            que.offer(progresses[i]);
-            speedQue.offer(speeds[i]);
-        }
-        
-        while(!que.isEmpty()){
-            int count = 0;
-            while(day * speedQue.peek() + que.peek() >= 100){
-                que.poll();
-                speedQue.poll();
-                count++;
-                if(que.isEmpty()) break;
-            }
-            if(count > 0){
-                list.add(count);
-            }
-            day++;
-        }
-        
-        answer = new int[list.size()];
-        
-        for(int i = 0; i < list.size(); i++){
-            answer[i] = list.get(i);
-        }
-        
+        int[] answer = {};
         return answer;
     }
 }
