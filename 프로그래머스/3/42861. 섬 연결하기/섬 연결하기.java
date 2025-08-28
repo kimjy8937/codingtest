e76227942864941f1235308a38a6cb1851cb779b
Kim JinYoung <42676165+kimjy8937@users.noreply.github.com>
2025-08-29 08:54:09 +0900

    [level 3] Title: 섬 연결하기, Time: 0.61 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,33 +1,6 @@
-import java.util.Arrays;
-import java.util.Comparator;
-
 class Solution {
-	static int[] island;
-	public static int findPoint(int a) {
-		if(island[a] == a)
-			return a;
-		else
-			return findPoint(island[a]);
-	}
-	public static int solution(int n, int[][] costs) {
-		Arrays.sort(costs, new Comparator<int []>() {
-			@Override
-			public int compare(int o1[], int o2[]) {
-				return o1[2] - o2[2];
-			}
-		});
-		int ans=0;
-		island = new int[n];
-		for(int i=0;i<n;i++)	island[i]=i;
-		for(int[] i : costs) {
-			int a = findPoint(i[0]);
-			int b = findPoint(i[1]);
-			if(a != b) {	
-				ans+=i[2];
-				island[a] = b;	
-			}
-				
-		}		
-		return ans;
-	}
+    public int solution(int n, int[][] costs) {
+        int answer = 0;
+        return answer;
+    }
 }
