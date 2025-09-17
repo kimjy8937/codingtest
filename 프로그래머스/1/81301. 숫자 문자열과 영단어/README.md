6d376fcf014ca675b8db2eba722ce5b06c3ee187
Kim JinYoung <42676165+kimjy8937@users.noreply.github.com>
2025-09-17 15:59:50 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.71 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/README.md
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 숫자 문자열과 영단어 - 81301 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301) 
 
 ### 성능 요약
 
-메모리: 80.3 MB, 시간: 0.11 ms
+메모리: 75.8 MB, 시간: 0.71 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 11일 14:04:02
+2025년 09월 17일 15:59:45
 
 ### 문제 설명
 
--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,54 +1,9 @@
-import java.util.Map;
-import java.util.HashMap;
-
 class Solution {
     public int solution(String s) {
-        int answer = 0;
-        HashMap<String, Integer> hs = new HashMap<>();
-        
-        hs.put("zero", 0);
-        hs.put("one", 1);
-        hs.put("two", 2);
-        hs.put("three", 3);
-        hs.put("four", 4);
-        hs.put("five", 5);
-        hs.put("six", 6);
-        hs.put("seven", 7);
-        hs.put("eight", 8);
-        hs.put("nine", 9);
-   
-        
-        StringBuilder sb = new StringBuilder();
-        
-        
-        char[] ca = s.toCharArray();
-        for(int i = 0; i < ca.length; i++){
-            if(ca[i] >= '0' && ca[i] <= '9'){
-                if(answer == 0)
-                    answer += (ca[i] - '0');
-                else{
-                    answer *= 10;
-                    answer += (ca[i] - '0');
-                }
-               
-            }
-            else {
-                sb.append(ca[i]);
-            }
-            
-            String str = sb.toString();
-            if(hs.containsKey(str)){
-                if(answer == 0)
-                    answer += hs.get(str);
-                else{
-                    answer *= 10;
-                    answer += hs.get(str);
-                }
-                sb.setLength(0);
-            }
+        String[] strArr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        for(int i = 0; i < strArr.length; i++) {
+            s = s.replaceAll(strArr[i], Integer.toString(i));
         }
-        
-        
-        return answer;
+        return Integer.parseInt(s);
     }
 }
