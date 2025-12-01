ceaf514f41014ac312e621b527707f1cf739f844
Kim JinYoung <42676165+kimjy8937@users.noreply.github.com>
2025-12-02 03:49:00 +0900

    [level 3] Title: 숫자 야구, Time: 16.24 ms, Memory: 86.8 MB -BaekjoonHub

--- 프로그래머스/3/451808. 숫자 야구/숫자 야구.java
+++ 프로그래머스/3/451808. 숫자 야구/숫자 야구.java
@@ -1,46 +1,51 @@
-public class Solution {
-    public int solution(int n, java.util.function.Function<Integer, String> submit) {
-        // 가능한 모든 후보 1000~9999 중, 1~9 4개 서로 다르고 0 미포함
-        java.util.List<Integer> candidates = new java.util.ArrayList<>();
-        for (int a = 1; a <= 9; a++) {
-            for (int b = 1; b <= 9; b++) {
-                if (b == a) continue;
-                for (int c = 1; c <= 9; c++) {
-                    if (c == a || c == b) continue;
-                    for (int d = 1; d <= 9; d++) {
-                        if (d == a || d == b || d == c) continue;
-                        candidates.add(1000 * a + 100 * b + 10 * c + d);
-                    }
-                }
-            }
-        }
-        while (candidates.size() > 1) {
-            // 단순히 리스트의 첫 번째를 guess로 사용
-            int guess = candidates.get(0);
-            String res = submit.apply(guess);
-            // "xS yB" 형식 가정
-            String[] parts = res.split(" ");
-            int strike = Integer.parseInt(parts[0].substring(0, parts[0].length()-1));
-            int ball   = Integer.parseInt(parts[1].substring(0, parts[1].length()-1));
+import java.util.Stack;
 
-            java.util.List<Integer> next = new java.util.ArrayList<>();
-            for (int cand : candidates) {
-                String s1 = String.valueOf(guess);
-                String s2 = String.valueOf(cand);
-                int st = 0, b = 0;
-                for (int i = 0; i < 4; i++) {
-                    if (s1.charAt(i) == s2.charAt(i)) {
-                        st++;
-                    } else if (s2.indexOf(s1.charAt(i)) != -1) {
-                        b++;
-                    }
-                }
-                if (st == strike && b == ball) {
-                    next.add(cand);
-                }
-            }
-            candidates = next;
-        }
-        return candidates.get(0);
-    }
-}
+ class Solution {
+     public void makeNum(Stack st){//중복없는 3자리 숫자 생성
+         for(int i=1;i<=9;i++){
+             for(int j=1;j<=9;j++){
+                 for(int k=1;k<=9;k++){
+                     if(i!=j&&j!=k&&k!=i){
+                         st.push(String.valueOf(100*i+10*j+k));
+                     }
+                 }
+             }
+         }
+     }
+     public boolean check(String num, int[][] baseball){
+         String base="";
+         int strike=0, ball=0;
+         for(int i=0;i<baseball.length;i++){
+             base=String.valueOf(baseball[i][0]);
+             for(int j=0;j<3;j++){
+                 if(num.charAt(j)==base.charAt(j)){//스트라이크 체크
+                     strike++;
+                 }
+                 if(base.contains(String.valueOf(num.charAt(j)))){//볼 체크
+                     ball++;
+                 }
+             }
+             ball=ball-strike;//스트라이크와 볼은 겹치지 않으므로
+             if(strike!=baseball[i][1]||ball!=baseball[i][2]){
+                 return false;
+             }
+             strike=0;
+             ball=0;
+         }
+         return true;
+     }
+     public int solution(int[][] baseball) {
+         int answer = 0;
+         Stack<String> st=new Stack<>();
+         String num;
+         boolean chk=false;
+         makeNum(st);
+         while(!st.isEmpty()){
+             num=st.pop();
+             if(check(num, baseball)){
+                 answer++;
+             }
+         }
+         return answer;
+     }
+ }
