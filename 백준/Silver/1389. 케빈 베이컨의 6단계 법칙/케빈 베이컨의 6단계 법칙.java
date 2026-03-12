62e437f2da025ccadce55fc579a087daaabde0b0
Kim JinYoung <42676165+kimjy8937@users.noreply.github.com>
2026-03-12 10:47:02 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 124 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -5,59 +5,60 @@ public class Main{
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
     static int N, M;
-    static ArrayList<Integer>[] graph;
+    static int[][] dist;
+    static final int INF = 1000000;
     
     public static void main(String[] args) throws Exception{
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         
-        graph = new ArrayList[N+1];
-        for (int i = 1; i <= N; i++) {
-            graph[i] = new ArrayList<>();
+        dist = new int[N+1][N+1];
+        
+        for(int i = 1; i <= N; i++){
+            for(int j = 1; j <= N; j++){
+                if(i == j){
+                    dist[i][j] = 0;
+                }else{
+                    dist[i][j] = INF;
+                }
+            }
         }
         
+
         for(int i = 0; i < M; i++){
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            graph[a].add(b);
-            graph[b].add(a);
+            dist[a][b] = 1;
+            dist[b][a] = 1;
+        }
+        
+        for(int k = 1; k <= N; k++){
+            for(int i = 1; i <= N; i++){
+                for(int j = 1; j<= N; j++){
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
         }
         
         int answer = 0;
         int min = Integer.MAX_VALUE;
+
         for(int i = 1; i <= N; i++){
-            int sum = bfs(i);
-            if(min > sum){
+
+            int sum = 0;
+
+            for(int j = 1; j <= N; j++){
+                sum += dist[i][j];
+            }
+
+            if(sum < min){
                 min = sum;
                 answer = i;
             }
         }
+
         System.out.print(answer);
-    }
-    
-    static int bfs(int start){
-        Queue<Integer> q = new LinkedList<>();
-        boolean[] visited = new boolean[N+1];
-        int[] dist = new int[N+1];
-        
-        q.offer(start);
-        visited[start] = true;
-        while(!q.isEmpty()){
-            int now = q.poll();
-            for(int num : graph[now]){
-                if(!visited[num]){
-                    visited[num] = true;
-                    q.offer(num);
-                    dist[num] = dist[now] + 1;
-                }
-            }
-        }
-        int sum = 0;
-        for(int i = 1; i <= N; i++){
-            sum += dist[i];
-        }
-        return sum;
     }
 }
