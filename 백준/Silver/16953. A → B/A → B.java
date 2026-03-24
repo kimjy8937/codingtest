ced9f5d80cfec651832fc0c33bf085b9f31f4b36
Kim JinYoung <42676165+kimjy8937@users.noreply.github.com>
2026-03-24 09:41:27 +0900

    [Silver II] Title: A → B, Time: 104 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.java
+++ 백준/Silver/16953. A → B/A → B.java
@@ -2,14 +2,6 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	
-	static class Node {
-		long n; int count;
-		Node(long n, int count) {
-			this.n = n;
-			this.count = count;
-		}
-	}
     public static void main(String[] args) throws Exception{
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	StringTokenizer st = new StringTokenizer(br.readLine());
@@ -17,25 +9,24 @@ public class Main {
     	long A = Long.parseLong(st.nextToken());
     	long B = Long.parseLong(st.nextToken());
     	
-    	int answer = -1;
+    	int answer = 1;
     	
-    	Queue<Node> q = new LinkedList<>();
-    	q.offer(new Node(A, 1));
-    	
-    	while(!q.isEmpty()) {
-    		Node now = q.poll();
-    		if(now.n == B) {
-    			answer = now.count;
-    			break;
-    		}
-    		if(now.n * 2 <= B) {
-    			q.offer(new Node(now.n * 2, now.count + 1));
-    		}
-    		if((now.n * 10 + 1) <= B) {
-    			q.offer(new Node(now.n * 10 + 1, now.count + 1));
-    		}
+    	while(B > A) {
+    		if(B % 2 == 0) {
+    			B /= 2;
+    			answer++;
+    		}else if(B > 10 && B % 10 == 1) {
+    			B /= 10;
+    			answer++;
+    		}else{
+              break;
+            }
     	}
     	
-    	System.out.println(answer);
+    	if(B == A) {
+    		System.out.println(answer);
+    	}else {
+    		System.out.println(-1);
+    	}
     }
 }
