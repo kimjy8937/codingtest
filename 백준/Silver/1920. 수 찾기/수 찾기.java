323942d5ce96e6a83577f7fd93fedd674ac7efb4
Kim JinYoung <42676165+kimjy8937@users.noreply.github.com>
2026-03-26 14:40:22 +0900

    [Silver IV] Title: 수 찾기, Time: 580 ms, Memory: 49672 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        Set<Integer> numSet = new HashSet<>();
+        
+        int N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            numSet.add(Integer.parseInt(st.nextToken()));
+        }
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < M; i++){
+            if(numSet.contains(Integer.parseInt(st.nextToken()))){
+                sb.append(1).append('\n');
+            }else{
+                sb.append(0).append('\n');
+            }
+        }
+        System.out.print(sb.toString());
+    }
+}
