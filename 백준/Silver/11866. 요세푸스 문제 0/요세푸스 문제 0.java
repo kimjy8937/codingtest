ea0403a8871fa57ad14312a0a5a68e5e75af0aae
Kim JinYoung <42676165+kimjy8937@users.noreply.github.com>
2026-03-28 00:36:56 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 128 ms, Memory: 15604 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        
+        Queue<Integer> q = new ArrayDeque<>();
+        
+        for(int i = 1; i <= N; i++){
+            q.offer(i);
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        sb.append('<');
+        while(!q.isEmpty()){
+            for(int i = 0; i < K-1; i ++){
+                q.offer(q.poll());
+            }
+            sb.append(q.poll());
+            if(!q.isEmpty()){
+                sb.append(", ");
+            }
+        }
+        
+        sb.append('>');
+        System.out.println(sb.toString());
+    }
+}
