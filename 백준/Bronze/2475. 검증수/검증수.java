f38e81a5ff7cf0efc16a007855516cd1e117b0e4
Kim JinYoung <42676165+kimjy8937@users.noreply.github.com>
2026-03-27 09:25:15 +0900

    [Bronze V] Title: 검증수, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	
+    	int sum = 0;
+    	while(st.hasMoreTokens()) {
+    		int num = Integer.parseInt(st.nextToken());
+    		sum += num * num;
+    	}
+    	System.out.println(sum % 10);
+    }
+}
